8df2c96a20a273464b178511cb135981d68d3adc
jotace06 <jjch422@naver.com>
2026-04-03 13:11:33 +0900

    chore: docker 설정 (#25)
    * chore: 배포하기 위한 파일 추가
    
    * chore: add gen ai version
    
    * fix: reroute 로직 수정
    
    * chore: add .example

--- apps/frontend/src/api/client.js
+++ apps/frontend/src/api/client.js
@@ -11,10 +11,6 @@ const client = axios.create({
 client.interceptors.response.use(
   (response) => response,
   (error) => {
-    const isAuthEndpoint = error.config?.url?.startsWith('/auth/');
-    if ((error.response?.status === 401 || error.response?.status === 403) && !isAuthEndpoint) {
-      window.location.href = '/signin';
-    }
     return Promise.reject(error);
   },
 );
