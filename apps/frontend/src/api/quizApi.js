905552cdd4fdf2ec007890e3d22c1b751ad944fd
jotace06 <jjch422@naver.com>
2026-04-04 16:52:18 +0900

    Revert "[배포] develop -> main (#26)"
    This reverts commit eeb37eca322c60a7330cda1628c4f98f3857b719.

--- apps/frontend/src/api/quizApi.js
+++
@@ -1,7 +0,0 @@
-import client from './client';
-
-export const createQuizSet = (content) => client.post('/quiz-sets', { content });
-
-export const getQuizSets = () => client.get('/quiz-sets');
-
-export const getQuizSet = (quizSetId) => client.get(`/quiz-sets/${quizSetId}`);
