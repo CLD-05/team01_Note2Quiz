d6895da564deb309c1d5bc57cf62e9767f5f9dcb
jotace06 <jjch422@naver.com>
2026-04-04 17:02:18 +0900

    Revert "[배포] develop -> main (#26)" (#27)
    This reverts commit eeb37eca322c60a7330cda1628c4f98f3857b719.

--- apps/backend/src/main/java/com/note2quiz/backend/repository/QuizRepository.java
+++
@@ -1,7 +0,0 @@
-package com.note2quiz.backend.repository;
-
-import com.note2quiz.backend.entity.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
-}
